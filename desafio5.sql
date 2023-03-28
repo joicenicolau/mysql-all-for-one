f86af3f1f59f0669e0f17263231bc4de770bedd6
Joice Nicolau <joice_nicolau@hotmail.com>
2023-03-28 15:13:15 -0300

    Req 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+-- Observaçções: 
+-- Tanto o quarto quanto o décimo terceiro registros, precisam aparecer na consulta;
+-- Não use where ou order by.
+
+SELECT * FROM products LIMIT 10 OFFSET 3;
+-- o quanto que quero que apareça
+-- offset os próximos que quero
+

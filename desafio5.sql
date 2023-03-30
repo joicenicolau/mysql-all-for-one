78cb9f24c2c6fed06dfa22c52f4b451dae1ae099
Joice M de Souza Nicolau <112522719+joicenicolau@users.noreply.github.com>
2023-03-30 10:18:50 -0300

    Update desafio5.sql

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,1 @@
--- Observaçções: 
--- Tanto o quarto quanto o décimo terceiro registros, precisam aparecer na consulta;
--- Não use where ou order by.
-
 SELECT * FROM products LIMIT 10 OFFSET 3;
--- o quanto que quero que apareça
--- offset os próximos que quero (apartir do 4)
-

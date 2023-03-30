057682b0ee1874d1d33085e6761a1992806f0263
Joice M de Souza Nicolau <112522719+joicenicolau@users.noreply.github.com>
2023-03-30 10:17:44 -0300

    Update desafio21.sql

--- desafio21.sql
+++ desafio21.sql
@@ -1,4 +1,1 @@
--- consulta: https://blog.betrybe.com/sql-insert/
--- pós leitura, deu para concluir que inserir duas linhas com um único insert era só duplicar os valores no VALUE
-
 INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id) VALUE (69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129), (69, 80, 15.0000, 15.000, 0, 2, NULL, NULL, 129);

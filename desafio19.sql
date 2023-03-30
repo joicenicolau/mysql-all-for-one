458de81a1ae5a82bce6379d7c9d705536a9f6f01
Joice M de Souza Nicolau <112522719+joicenicolau@users.noreply.github.com>
2023-03-30 10:16:57 -0300

    Update desafio19.sql

--- desafio19.sql
+++ desafio19.sql
@@ -1,3 +1,1 @@
 SELECT COUNT(*) AS 'orders_count' FROM orders WHERE employee_id = 5 OR employee_id = 6 AND shipper_id = 2;
--- COUNT(*) = conta todos os pedidos
--- AS = nomeia os resultados para uma tabela orders_coun
